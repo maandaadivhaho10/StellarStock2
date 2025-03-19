6f887157df0365b1efffba704f8870775c80477c
maanda adivhaho <maandaadivhaho74@gmail.com>
2025-03-19 08:08:54 +0200

    first commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Import your main app component
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Get the root element where your app will be rendered
+const rootElement = document.getElementById('root');
+
+// Create a root and render the App component inside it
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
