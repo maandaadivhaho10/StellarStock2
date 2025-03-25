16432f89d5cda97265e65408035ce72f1e862b10
maanda adivhaho <maandaadivhaho74@gmail.com>
2025-03-25 13:57:26 +0200

    Add icons for pages

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavBar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaSignInAlt, FaStore, FaUserPlus, FaShoppingCart } from "react-icons/fa";
+import "./NavBar.css";
 
 function NavBar({ cartItemCount }) {
-    return (
-      <>
-      <nav className="navbar">
+  return (
+    <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-brand">StellarStock</Link>
+        <Link to="/" className="navbar-brand">
+          StellarStock
+        </Link>
         <div className="navbar-links">
-          <Link to="/">Home</Link>
-          <Link to="/Login">Login</Link>
-          <Link to="/Sell">Sell on StellarStocks</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/cart" className="cart-link">🛒 Cart ({cartItemCount})</Link>
+          <Link to="/">
+            <FaHome /> Home
+          </Link>
+          <Link to="/login">
+            <FaSignInAlt /> Login
+          </Link>
+          <Link to="/sell">
+            <FaStore /> Sell on StellarStocks
+          </Link>
+          <Link to="/signup">
+            <FaUserPlus /> Sign Up
+          </Link>
+          <Link to="/cart" className="cart-link">
+            <FaShoppingCart /> Cart ({cartItemCount})
+          </Link>
         </div>
       </div>
     </nav>
-      </>
-    )
-  }
-  
-  export default NavBar;
+  );
+}
+
+export default NavBar;
