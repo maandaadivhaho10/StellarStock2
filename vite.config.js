b47ffab1a1df1798a5a690f00f71c0eb2f7d0cab
adivhaho <maandaadivhaho74@gmail.com>
2026-01-20 02:22:46 +0200

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/StellarStock2/',  // replace with your repository name
+  plugins: [react()]  // replace with your repository name
 });
